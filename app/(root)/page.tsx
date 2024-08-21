5a9d82b4c87737220a7e06061909ae64eb98117a
bregarulai <bregarulai@gmail.com>
2024-08-21 11:58:14 -0400

    implement PostEditor

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import PostEditor from "@/components/posts/editor/PostEditor";
 
 export default function Home() {
   return (
     <main className="h-[200vh] w-full">
-      <div className="w-full">Home page</div>
+      <div className="w-full">
+        <PostEditor />
+      </div>
     </main>
   );
 }
