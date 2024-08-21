cae5a205b008963813f05b8f41492c79bd51e856
bregarulai <bregarulai@gmail.com>
2024-08-20 23:33:55 -0400

    implement logo link in header

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,13 +3,21 @@ import React from "react";
 
 import { validateRequest } from "@/lucia/auth";
 import SessionProvider from "@/providers/SessionProvider";
+import Navbar from "@/components/Navbar";
 
 const RootLayout = async ({ children }: { children: React.ReactNode }) => {
   const session = await validateRequest();
 
   if (!session.user) redirect("/sign-in");
 
-  return <SessionProvider value={session}>{children}</SessionProvider>;
+  return (
+    <SessionProvider value={session}>
+      <div className="flex min-h-screen flex-col">
+        <Navbar />
+        <div className="mx-auto max-w-7xl p-5">{children}</div>
+      </div>
+    </SessionProvider>
+  );
 };
 
 export default RootLayout;
---
+++ components/Navbar.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <header className="sticky top-0 z-10 bg-card shadow-sm">
+      <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-center gap-5 px-5 py-3">
+        <Link href="/" className="text-2xl font-bold text-primary">
+          VibeHub
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
