ac4fc523be66e579d4fa8b5600c6a9b73cca1359
bregarulai <bregarulai@gmail.com>
2024-08-21 10:20:57 -0400

    implements navigation menu

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { validateRequest } from "@/lucia/auth";
 import SessionProvider from "@/providers/SessionProvider";
 import Navbar from "@/components/Navbar";
+import MenuBar from "@/components/MenuBar";
 
 const RootLayout = async ({ children }: { children: React.ReactNode }) => {
   const session = await validateRequest();
@@ -14,7 +15,11 @@ const RootLayout = async ({ children }: { children: React.ReactNode }) => {
     <SessionProvider value={session}>
       <div className="flex min-h-screen flex-col">
         <Navbar />
-        <div className="mx-auto max-w-7xl p-5">{children}</div>
+        <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
+          <MenuBar className="sticky top-[5.24rem] hidden h-fit flex-none scroll-py-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80" />
+          {children}
+        </div>
+        <MenuBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden" />
       </div>
     </SessionProvider>
   );
