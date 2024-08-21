d3e12d7c3fa8461af4f278617dd868fe5ebd6330
bregarulai <bregarulai@gmail.com>
2024-08-21 10:31:26 -0400

    implements post model

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main>Home page</main>;
+  return (
+    <main className="h-[200vh] w-full">
+      <div className="w-full">Home page</div>
+    </main>
+  );
 }
