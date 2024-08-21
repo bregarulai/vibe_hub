ce65921b642928757ae52eb9139fcbd413942176
bregarulai <bregarulai@gmail.com>
2024-08-20 23:17:44 -0400

    implement session provider

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,13 +2,14 @@ import { redirect } from "next/navigation";
 import React from "react";
 
 import { validateRequest } from "@/lucia/auth";
+import SessionProvider from "@/providers/SessionProvider";
 
 const RootLayout = async ({ children }: { children: React.ReactNode }) => {
   const session = await validateRequest();
 
   if (!session.user) redirect("/sign-in");
 
-  return <>{children}</>;
+  return <SessionProvider value={session}>{children}</SessionProvider>;
 };
 
 export default RootLayout;
