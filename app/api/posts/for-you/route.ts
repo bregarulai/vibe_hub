f3dd142366d7351cdc0f65fe2dcafe92646d908b
bregarulai <bregarulai@gmail.com>
2024-08-24 10:34:04 -0400

    implement route and post repository to return infinite scroll data

--- app/api/posts/for-you/route.ts
+++ app/api/posts/for-you/route.ts
@@ -1,15 +1,29 @@
+import { NextRequest } from "next/server";
+
 import { validateRequest } from "@/lucia/auth";
 import { getForYouPosts } from "@/server/repositories/post.repository";
+import { PostsPage } from "@/lib/type";
 
-export async function GET() {
+export async function GET(req: NextRequest) {
   try {
+    const cursor = req.nextUrl.searchParams.get("cursor") || undefined;
+
+    const pageSize = 10;
+
     const { user } = await validateRequest();
 
     if (!user) return Response.json({ error: "Unauthorized" }, { status: 401 });
 
-    const posts = await getForYouPosts();
+    const posts = await getForYouPosts({ pageSize, cursor });
 
-    return Response.json(posts);
+    const nextCursor = posts.length > pageSize ? posts[pageSize].id : null;
+
+    const data: PostsPage = {
+      posts: posts.slice(0, pageSize),
+      nextCursor,
+    };
+
+    return Response.json(data);
   } catch (error) {
     console.log(error);
     return Response.json({ error: "Internal Server Error" }, { status: 500 });
--- server/repositories/post.repository.ts
+++ server/repositories/post.repository.ts
@@ -1,11 +1,21 @@
 import prisma from "@/lib/prisma";
 import { postDataInclude } from "@/lib/type";
 
-export const getForYouPosts = async () => {
+type GetForYouPostsParams = {
+  pageSize: number;
+  cursor: string | undefined;
+};
+
+export const getForYouPosts = async ({
+  pageSize,
+  cursor,
+}: GetForYouPostsParams) => {
   try {
     const posts = await prisma.post.findMany({
       include: postDataInclude,
       orderBy: { createdAt: "desc" },
+      take: pageSize + 1,
+      cursor: cursor ? { id: cursor } : undefined,
     });
 
     return posts;
