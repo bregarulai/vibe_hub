48b0b3265d518411b7e934d6216ed83a58f760ff
bregarulai <bregarulai@gmail.com>
2024-08-24 14:50:17 -0400

    implement PostMoreButton component

---
+++ components/posts/PostMoreButton.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useState } from "react";
+
+import { PostData } from "@/lib/type";
+import DeletePostDialog from "@/components/posts/DeletePostDialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { MoreHorizontal, Trash2 } from "lucide-react";
+
+type PostMoreButtonProps = {
+  post: PostData;
+  className?: string;
+};
+
+const PostMoreButton = ({ post, className }: PostMoreButtonProps) => {
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            size="icon"
+            variant="ghost"
+            className={className}
+            aria-label="More Options"
+          >
+            <MoreHorizontal className="size-5 text-muted-foreground" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem onClick={() => setShowDeleteDialog(true)}>
+            <span className="flex items-center gap-3 text-destructive">
+              <Trash2 className="size-4" />
+              Delete
+            </span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DeletePostDialog
+        post={post}
+        open={showDeleteDialog}
+        onClose={() => setShowDeleteDialog(false)}
+      />
+    </>
+  );
+};
+
+export default PostMoreButton;
