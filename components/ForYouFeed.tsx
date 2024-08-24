4a660def5d5ab169f2c02f6f1230a2801b2cdcf7
bregarulai <bregarulai@gmail.com>
2024-08-24 10:50:22 -0400

    implement infinite scross in ForYouFeed component

--- components/ForYouFeed.tsx
+++ components/ForYouFeed.tsx
@@ -1,24 +1,42 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { Loader2 } from "lucide-react";
 
-import { PostData } from "@/lib/type";
+import { PostsPage } from "@/lib/type";
 import { FOR_YOU, POST_FEED } from "@/lib/constants";
 import Post from "@/components/posts/Post";
 import kyInstance from "@/lib/ky";
+import { Button } from "./ui/button";
 
 const ForYouFeed = () => {
-  const query = useQuery<PostData[]>({
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
     queryKey: [POST_FEED, FOR_YOU],
-    queryFn: kyInstance.get("/api/posts/for-you").json<PostData[]>,
+    queryFn: ({ pageParam }) =>
+      kyInstance
+        .get(
+          "/api/posts/for-you",
+          pageParam ? { searchParams: { cursor: pageParam } } : {},
+        )
+        .json<PostsPage>(),
+    initialPageParam: null as string | null,
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
   });
 
-  if (query.status === "pending") {
+  const posts = data?.pages.flatMap((page) => page.posts) || [];
+
+  if (status === "pending") {
     return <Loader2 className="mx-auto animate-spin" />;
   }
 
-  if (query.status === "error") {
+  if (status === "error") {
     return (
       <p className="text-center text-destructive">
         An error occurred while loading posts.
@@ -28,9 +46,10 @@ const ForYouFeed = () => {
 
   return (
     <div className="space-y-5">
-      {query.data.map((post) => (
+      {posts.map((post) => (
         <Post key={post.id} post={post} />
       ))}
+      <Button onClick={() => fetchNextPage()}>Load More</Button>
     </div>
   );
 };
