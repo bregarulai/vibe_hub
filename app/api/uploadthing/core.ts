6212031764bcb2527513e816a7f358a8971f3df6
bregarulai <bregarulai@gmail.com>
2024-08-30 10:42:12 -0400

    implement deleting avatar image if one exist when uploading avatar

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,5 +1,5 @@
 import { createUploadthing, FileRouter } from "uploadthing/next";
-import { UploadThingError } from "uploadthing/server";
+import { UploadThingError, UTApi } from "uploadthing/server";
 
 import { validateRequest } from "@/lucia/auth";
 import { updateUserAvatar } from "@/server/repositories/user.repository";
@@ -17,6 +17,15 @@ export const fileRouter = {
       return { user };
     })
     .onUploadComplete(async ({ metadata, file }) => {
+      const oldAvatarUrl = metadata.user.avatarUrl;
+
+      if (oldAvatarUrl) {
+        const key = oldAvatarUrl.split(
+          `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`,
+        )[1];
+
+        await new UTApi().deleteFiles(key);
+      }
       const newAvatarUrl = file.url.replace(
         "/f/",
         `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`,
