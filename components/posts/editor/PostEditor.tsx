144539befdb8befbfff64b56734745ecce98fb3e
bregarulai <bregarulai@gmail.com>
2024-09-01 12:27:54 -0400

    implement copy paste support for files

--- components/posts/editor/PostEditor.tsx
+++ components/posts/editor/PostEditor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef } from "react";
+import { ClipboardEvent, useRef } from "react";
 import Image from "next/image";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
@@ -74,17 +74,26 @@ const PostEditor = () => {
     );
   };
 
+  const onPaste = (e: ClipboardEvent<HTMLInputElement>) => {
+    const files = Array.from(e.clipboardData?.items || [])
+      .filter((item) => item.kind === "file")
+      .map((item) => item.getAsFile()) as File[];
+
+    startUpload(files);
+  };
+
   return (
     <div className="flex flex-col gap-5 rounded-2xl bg-card p-5 shadow-sm">
       <div className="flex gap-5">
         <UserAvatar avatarUrl={user.avatarUrl} className="hidden sm:inline" />
         <div {...rootProps} className="w-full">
           <EditorContent
+            editor={editor}
+            onPaste={onPaste}
             className={cn(
               "max-h-[20rem] w-full overflow-y-auto rounded-2xl bg-background px-5 py-3",
               isDragActive && "outline-dashed outline-primary/50",
             )}
-            editor={editor}
           />
           <input {...getInputProps()} />
         </div>
