71e1202e186a9e4cc33e035dea0e2efcb8b215eb
bregarulai <bregarulai@gmail.com>
2024-09-01 12:21:16 -0400

    implement file drag and drop

--- components/posts/editor/PostEditor.tsx
+++ components/posts/editor/PostEditor.tsx
@@ -6,6 +6,7 @@ import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Placeholder from "@tiptap/extension-placeholder";
 import { ImageIcon, Loader2, X } from "lucide-react";
+import { useDropzone } from "@uploadthing/react";
 
 import "./style.css";
 
@@ -30,6 +31,13 @@ const PostEditor = () => {
     uploadProgress,
     isUploading,
   } = useMediaUpload();
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    multiple: true,
+    onDrop: startUpload,
+  });
+
+  const { onClick, ...rootProps } = getRootProps();
 
   const editor = useEditor({
     extensions: [
@@ -70,10 +78,16 @@ const PostEditor = () => {
     <div className="flex flex-col gap-5 rounded-2xl bg-card p-5 shadow-sm">
       <div className="flex gap-5">
         <UserAvatar avatarUrl={user.avatarUrl} className="hidden sm:inline" />
-        <EditorContent
-          className="max-h-[20rem] w-full overflow-y-auto rounded-2xl bg-background px-5 py-3"
-          editor={editor}
-        />
+        <div {...rootProps} className="w-full">
+          <EditorContent
+            className={cn(
+              "max-h-[20rem] w-full overflow-y-auto rounded-2xl bg-background px-5 py-3",
+              isDragActive && "outline-dashed outline-primary/50",
+            )}
+            editor={editor}
+          />
+          <input {...getInputProps()} />
+        </div>
       </div>
       {!!attachments.length && (
         <AttachmentPreviews
